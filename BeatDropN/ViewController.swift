d968696117b0b960e3be3a5f2a803ebb8e9f3d8c
Brian Tang <brianytang@gmail.com>
2016-07-09 15:57:23 -0700

    Initial Commit

---
+++ BeatDropN/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BeatDropN
+//
+//  Created by Brian Tang on 7/9/16.
+//  Copyright © 2016 bytLabs. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
